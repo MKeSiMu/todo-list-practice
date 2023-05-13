4eb1de4b8d96198f67447ddfbf0e71088ab2c6dd
Maksym Maksimov <maksymmaksimov88@gmail.com>
2023-05-13 17:41:14 +0800

    Setup models for todo-list-practice project and made migrations

--- todo_list_app/models.py
+++ todo_list_app/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=60)
+
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+    TASK_STATUS_CHOICES = [(False, 'Not done'), (True, 'Done')]
+    content = models.CharField(max_length=250)
+    created_datetime = models.DateTimeField(auto_now_add=True)
+    deadline_datetime = models.DateTimeField(
+        default=timezone.now,
+        null=True,
+        blank=True,
+        help_text="Enter date and time in '2023-05-13 06:31' format"
+    )
+    task_status = models.BooleanField(default=False, choices=TASK_STATUS_CHOICES)
+    tag = models.ManyToManyField(Tag, related_name="tasks")
+
+    class Meta:
+        ordering = ["task_status", "-created_datetime"]
+
+    def __str__(self):
+        return self.content
